d9788384ef1049accd6f5a58e26bc5b3bc9ed06e
Narottamkumar998 <narottamk998@gmail.com>
2025-08-01 13:21:36 +0530

    Add files via upload

---
+++ qr_code.py
@@ -0,0 +1,4 @@
+import qrcode
+qr_image=qrcode.make(" i, I'm Ch. Narottam Kumar, a B.Tech Computer Science student at ITM Aligarh. I'm skilled in Python, HTML, CSS ,and C programming, a " \
+"I’m passionate about coding and continuously learning new technologies to grow as a developer. ")
+qr_image.save("qrcode.png")
